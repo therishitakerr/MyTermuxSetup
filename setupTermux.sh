c12d8c679c7ee5cb98f03a8a0e85c8e6fda4305f
therishitakerr <75129590+therishitakerr@users.noreply.github.com>
2023-03-17 13:02:23 +0000

    Update setupTermux.sh

--- setupTermux.sh
+++ setupTermux.sh
@@ -24,7 +24,7 @@ echo 'HOMEDIR="/data/data/com.termux/files/home"' >> /data/data/com.termux/files
 echo 'alias updateytdlp="pip install --upgrade yt-dlp"' >> /data/data/com.termux/files/usr/etc/bash.bashrc
 echo 'alias gotodownloads="cd /data/data/com.termux/files/home/storage/downloads"' >> /data/data/com.termux/files/usr/etc/bash.bashrc
 echo 'alias editalias="vim /data/data/com.termux/files/usr/etc/bash.bashrc"' >> /data/data/com.termux/files/usr/etc/bash.bashrc
-echo 'alias video1080p="yt-dlp -i --add-metadata --embed-thumbnail --embed-subs --sub-lang en -f 'bestaudio+bestvideo[height=1080]'"' >> /data/data/com.termux/files/usr/etc/bash.bashrc
-echo 'alias video720p="yt-dlp -i --add-metadata --embed-thumbnail --embed-subs --sub-lang en -f 'bestaudio+bestvideo[height=720]'"' >> /data/data/com.termux/files/usr/etc/bash.bashrc
-echo 'alias video480p="yt-dlp -i --add-metadata --embed-thumbnail --embed-subs --sub-lang en -f 'bestaudio+bestvideo[height=480]'"' >> /data/data/com.termux/files/usr/etc/bash.bashrc
+echo 'alias video1080p="yt-dlp -i --add-metadata --embed-thumbnail --embed-subs --sub-langs all -f 'bestaudio+bestvideo[height=1080]'"' >> /data/data/com.termux/files/usr/etc/bash.bashrc
+echo 'alias video720p="yt-dlp -i --add-metadata --embed-thumbnail --embed-subs --sub-langs all -f 'bestaudio+bestvideo[height=720]'"' >> /data/data/com.termux/files/usr/etc/bash.bashrc
+echo 'alias video480p="yt-dlp -i --add-metadata --embed-thumbnail --embed-subs --sub-langs all -f 'bestaudio+bestvideo[height=480]'"' >> /data/data/com.termux/files/usr/etc/bash.bashrc
 echo 'alias getaudio="yt-dlp -i --add-metadata --embed-thumbnail -x"' >> /data/data/com.termux/files/usr/etc/bash.bashrc
